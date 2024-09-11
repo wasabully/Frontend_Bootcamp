abc10f2ddd7eeff1aa185d66ca8e5b93fe701198
danil <danildemchenko.official@gmail.com>
2024-09-11 17:05:03 +0300

    removeReps

--- day00/src/chapter_3/removeReps.js
+++ day00/src/chapter_3/removeReps.js
@@ -1,6 +1,22 @@
 // Вам нужно написать функцию которая принимает в качестве аргумента массив чисел и удаляет все повторяющиеся значения.
 
-function removeReps(array) {}
+function removeReps(array) {
+	let uniqueValues = [];
 
-removeReps([1, 1, 2, 4, 5, 6, 6, 8, 9, 11]); // [1,2,4,5,6,8,9,11]
+	for (let i = 0; i < array.length; i++) {
+		let unique = true;
 
+		for (let j = 0; j < uniqueValues.length; j++) {
+			if (array[i] === uniqueValues[j]) {
+				unique = false;
+				break;
+			}
+		}
+		if (unique) {
+			uniqueValues.push(array[i]);
+		}
+	}
+	return uniqueValues;
+}
+
+console.log(removeReps([1, 1, 2, 4, 5, 6, 6, 8, 9, 11])); // [1,2,4,5,6,8,9,11]
